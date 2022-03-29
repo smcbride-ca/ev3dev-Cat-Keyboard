7d1c6e20d8010b013831a64e03037577684b5e19
smcbride-ca <74470556+smcbride-ca@users.noreply.github.com>
2022-03-29 11:46:17 -0500

    Update cat-keyboard.py
    Significant improvements to speed, and some small fixes

--- cat-keyboard.py
+++ cat-keyboard.py
@@ -3,19 +3,23 @@
 #Import the needed modules.
 #Time for getting the time and waiting, LargeMotor for motor control,
 # Leds for the leds, Sound for audio, and keyboard for input.
-import time
+from time import sleep
+from time import time
 from ev3dev2.motor import LargeMotor
-from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
-import keyboard
+from keyboard import on_press
+
+connected = False
 
 #Set up commands
-lm = LargeMotor()
-leds = Leds()
+while connected == False :
+    try :
+        lm = LargeMotor()
+        connected = True
+    except :
+        print("Please connect a large motor\n")
 sound = Sound()
 
-#Turn all lights off
-leds.all_off()
 
 #BEEP and then get user input on the number of rotations the motor will do.
 #The "Try" part trys to run that command
@@ -23,36 +27,37 @@ leds.all_off()
 #The "finally" part is what happens after.
 sound.beep()
 try:
-    rot = int(input("Rotation: "))
+    print("Rotation: ")
+    rot = int(input("\b"))
 except:
-    print("\rThere was an error. Please try again.")
-    rot = float(input("Rotation: "))
+    print("\rThere was an error. Please try again.\nRotation: ")
+    rot = float(input("\b"))
 finally:
     print("Rotation set successfully")
 
 #Set time variables
-curtime = (time.time())
-time.sleep(2.3)
-oldtime = (time.time())
+curtime = (time())
+sleep(2.3)
+oldtime = (time())
 
 #Define what happens when a key is pressed, 
 #while making sure that at least 2 seconds have passed
 def key_press(key):
     global curtime
     global oldtime
-    curtime = (time.time())
+    curtime = (time())
     if (curtime - 2) >= oldtime:
         lm.on_for_rotations(25, rot)
-        time.sleep(0.5)
-        oldtime = time.time()
+        sleep(0.5)
+        oldtime = time()
 
 #Start waiting for a key to be pressed. The (key_press) part is what happens,
 #see above for the key_press function
-keyboard.on_press(key_press)
+on_press(key_press)
 
 #Play a tune so that the user knows that the script is ready
 sound.tone([(493.88, 500, 500), (440, 500, 500), (523.25, 500, 500)])
 
 #Keep the program alive
 while True:
-    time.sleep(0.1)
+    pass
